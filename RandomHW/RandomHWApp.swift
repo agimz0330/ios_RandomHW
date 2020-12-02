a7af325344679cf157df595d5a146b5265e1d9bb
c60603 <c60603@gmail.com>
2020-12-02 14:43:55 +0800

    Initial Commit

---
+++ RandomHW/RandomHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomHWApp.swift
+//  RandomHW
+//
+//  Created by User08 on 2020/12/2.
+//
+
+import SwiftUI
+
+@main
+struct RandomHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
